c075717138e21bf84205b5e0a24e5695a5272dcd
mateus.santos <mateus.santos@cf.coop.br>
2024-11-04 11:24:55 -0300

    Funcionando com css

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
-const fs = require('fs')
-const { google } = require('googleapis')
+const express = require('express');
+const fs = require('fs');
+const { google } = require('googleapis');
+const multer = require('multer');
+const path = require('path');
 
-const GOOGLE_API_FOLDER_ID = '1graDV-yFauZMP-5oqY17hCJKfXtzxv4J';
+const app = express();
+const PORT = 3000;
 
-async function uploadFile(){
-    try{
+const GOOGLE_API_FOLDER_ID = '1eLLtsuoRs9y76QWnVjH1wfMbv8tDlvZ1';
+
+// Configuração do multer para upload de arquivos
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.static('public'));
+
+// Endpoint para upload de arquivos
+app.post('/upload', upload.single('file'), async (req, res) => {
+    const filePath = path.join(__dirname, req.file.path);
+    
+    try {
         const auth = new google.auth.GoogleAuth({
-            keyFile: './captureqrcode-8ba6998d36d5.json',
+            keyFile: './qrcode-440713-e63a543ff2ec.json',
             scopes: ['https://www.googleapis.com/auth/drive']
-        })
+        });
 
         const driveService = google.drive({
             version: 'v3',
             auth
-        })
+        });
 
         const fileMetaData = {
-            'name': 'titoplace.jpeg',
-            'parents': [GOOGLE_API_FOLDER_ID]
-        }
+            name: req.file.originalname,
+            parents: [GOOGLE_API_FOLDER_ID]
+        };
 
         const media = {
-            mimeType: 'image/jpeg',
-            body: fs.createReadStream('./tito.jpeg')
-        }
+            mimeType: req.file.mimetype,
+            body: fs.createReadStream(filePath)
+        };
 
         const response = await driveService.files.create({
             resource: fileMetaData,
             media: media,
-            field: 'id'
-        })
-        return response.data.id
+            fields: 'id'
+        });
 
-    }catch(err){
-        console.log('Upload file error', err)
+        fs.unlinkSync(filePath); // Remove o arquivo temporário após o upload
+        res.status(200).send(`Arquivo enviado com sucesso: ${response.data.id}`);
+    } catch (err) {
+        console.log('Upload file error', err);
+        res.status(500).send('Erro ao enviar o arquivo.');
     }
-}
+});
 
-uploadFile().then(data => {
-    console.log(data)
-    //https://drive.google.com/uc?export=view&id=
-})
+// Rota principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './', 'index.html'));
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
